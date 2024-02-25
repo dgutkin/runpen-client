9cd604e522eb2744096332f1c323b92a2f7ed712
Daniel Gutkin <dngutkin@gmail.com>
2024-02-24 22:15:14 -0500

    Change footer text

--- app/components/footer.js
+++ app/components/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return(
       <div className="bg-dark-green p-4 w-full fixed bottom-0">
         <div className="flex flex-row justify-between mx-4">
-          <p className="text-white text-xs">Developed by Dan Gutkin</p>
+          <p className="text-white text-xs">Proudly developed in Canada.</p>
         </div>
       </div>
     );
