f3c03ea31f4accff63ce3c4e7f3940459ac4e916
Daniel Gutkin <dngutkin@gmail.com>
2024-02-26 16:45:24 -0500

    Add wiggle animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,18 @@ module.exports = {
         "black": "#000000",
         "battleship-gray": "#798478",
         "yinmn-blue": "#36558f"
+      },
+      animation: {
+        wiggle: "wiggle 1s linear 1s infinite"
+      },
+      keyframes: {
+        wiggle: {
+          "0%": { transform: "rotate(0deg);" },
+          "25%": { transform: "rotate(3deg);" },
+          "50%": { transform: "rotate(0deg);" },
+          "75%": { transform: "rotate(-3deg);" },
+          "100%": { transform: "rotate(0deg);" }
+        }
       }
     }
   },
