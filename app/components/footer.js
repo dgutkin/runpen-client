94bde9a8fc374d63a572924bafc0ef65666db919
Daniel Gutkin <dngutkin@gmail.com>
2024-02-26 13:08:21 -0500

    Update footer text

--- app/components/footer.js
+++ app/components/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return(
       <div className="bg-dark-green p-4 w-full fixed bottom-0">
         <div className="flex flex-row justify-between mx-4">
-          <p className="text-white text-xs">Proudly developed in Canada.</p>
+          <p className="text-white text-xs">An indie development project.</p>
         </div>
       </div>
     );
