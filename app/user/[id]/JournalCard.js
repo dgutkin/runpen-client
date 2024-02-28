1b1892a25365a4d0d943bd05302cc23fd59f1447
Daniel Gutkin <dngutkin@gmail.com>
2024-02-28 14:14:12 -0500

    Update journal card design

--- app/user/[id]/JournalCard.js
+++ app/user/[id]/JournalCard.js
@@ -9,14 +9,16 @@ export function JournalCard({data, editJournal, setJournalInFocus, setShowDelete
 
   return (
 
-    <div className="w-[30%] mx-1 my-2 rounded-xl border shadow-md flex flex-row hover:animate-wiggle">
+    <div className="w-[25%] h-64 mx-6 my-2 rounded-lg border shadow-xl hover:animate-wiggle">
+      
       <button 
-        className="w-full p-4" 
+        className="w-full h-full p-4 flex flex-col" 
         onClick={() => editJournal(data.journalId)}
       >
         <h3 className="text-md font-bold text-start">{data.journalName}</h3>
-        <p className="text-sm text-start my-2">Created: {data.createdDate}</p>
+        <p className="text-sm text-start my-2">Created: {data.createdDate}</p> 
       </button>
+      
     </div>
   );
   
