055b1b365669c5ef332e121b5037798ff7440738
Daniel Gutkin <dngutkin@gmail.com>
2024-02-29 16:56:41 -0500

    Move cancel button up top for add entry form

--- app/journal/[id]/AddEntryForm.js
+++ app/journal/[id]/AddEntryForm.js
@@ -2,10 +2,12 @@
 
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 
-function AddEntryForm({addEntry, setShowAddEntry, journalId}) {
+function AddEntryForm({ newEntryDate, addEntry, setShowAddEntry, journalId }) {
 
-    const [entryDate, setEntryDate] = useState("");
+    // const [entryDate, setEntryDate] = useState("");
     const [entryLabel, setEntryLabel] = useState("");
     const [entryEffort, setEntryEffort] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
@@ -14,10 +16,10 @@ function AddEntryForm({addEntry, setShowAddEntry, journalId}) {
       
       setErrorMessage("");
   
-      if (entryDate === "" || Date.parse(entryDate) > new Date()) {
-        setErrorMessage("Entry date is invalid.");
-        return;
-      } else if (entryLabel.length > 50) {
+      // if (entryDate === "" || Date.parse(entryDate) > new Date()) {
+      //   setErrorMessage("Entry date is invalid.");
+      //   return;
+      if (entryLabel.length > 50) {
         setErrorMessage("Label exceeds character limit.");
         return;
       } else if (entryEffort == "" || entryLabel == "") {
@@ -26,7 +28,7 @@ function AddEntryForm({addEntry, setShowAddEntry, journalId}) {
       }
   
       const data = {
-        entryDate: entryDate,
+        entryDate: newEntryDate,
         entryLabel: entryLabel,
         entryEffort: entryEffort,
         entryId: "E-" + uuidv4(),
@@ -40,38 +42,48 @@ function AddEntryForm({addEntry, setShowAddEntry, journalId}) {
     return (
       <div className="fixed inset-0 flex items-center justify-center">
         <div className="fixed inset-0 bg-black opacity-50"></div>
-        <div className="bg-white overflow-y-auto w-[45%] h-[55%] shadow-2xl border border-gray rounded-md p-4 m-10 z-10">
+        <div className="bg-white overflow-y-auto w-[50%] min-w-fit h-[50%] min-h-fit shadow-2xl border border-gray rounded-md p-4 m-10 z-10">
           <form className="flex flex-col">
-            <h2 className="text-xl font-semibold px-2">Add Entry</h2>
-            <div className="my-6 mx-6">
+            <div className="flex flex-row justify-between">
+              <h2 className="text-xl font-semibold px-2 mb-4">Add Entry</h2>
+              <button 
+                className="text-gray-500 px-2" 
+                type="button"
+                onClick={() => setShowAddEntry(false)}
+              >
+                  <FontAwesomeIcon icon={faX} size="lg"/>
+              </button>
+            </div>
+            <div className="my-4 mx-6 flex flex-row">
               <label 
                 htmlFor="date" 
                 className="text-md mb-2 mr-14"
               >
                 Date
               </label>
-              <input 
+              <p className="font-semibold">{newEntryDate.toLocaleString('en-us', {month: 'short', day: 'numeric', year: 'numeric'})}</p>
+              {/* <input 
                 type="date" 
                 id="date" 
                 name="date" 
-                className="w-[40%] bg-gray-100 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="w-[60%] bg-gray-100 border border-gray-200 text-gray-700 p-2 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 onInput={(e) => setEntryDate(e.target.value)}
-              />
+              /> */}
             </div>
 
-            <div className="m-6">
+            <div className="my-4 mx-6">
               <label htmlFor="label" className="text-md mb-2 mr-12">Label</label>
               <input
                 type="text"
                 id="label"
                 name="label"
-                className="p-2 border rounded-md w-1/2"
+                className="p-2 border rounded-md w-[80%]"
                 onInput={(e) => setEntryLabel(e.target.value)}
               />
               <p className="text-sm text-gray-200 ml-24">Character limit of 50.</p>
             </div>
   
-            <div className="my-6 mx-6">
+            <div className="my-4 mx-6">
               <label className="text-md mr-10">Intensity</label>
               <button 
                 className="text-black bg-gray-200 focus:bg-dark-green focus:text-white mx-1 p-2 rounded-md" 
@@ -96,21 +108,14 @@ function AddEntryForm({addEntry, setShowAddEntry, journalId}) {
               </button>
             </div>
   
-            <div className="flex flex-row my-6 mx-6">
-            <button 
-              className="bg-dark-green text-white w-[25%] px-4 py-2 mt-6 mx-2 rounded-md hover:bg-yinmn-blue" 
-              type="button"
-              onClick={submitEntry}
-            >
-              Create Entry
-            </button>
-            <button 
-              className="bg-dark-green text-white w-[25%] px-4 py-2 mt-6 mx-2 rounded-md hover:bg-yinmn-blue" 
-              type="button"
-              onClick={() => setShowAddEntry(false)}
-            >
-              Cancel
-            </button>
+            <div className="flex flex-row my-6 mx-8 justify-end">
+              <button 
+                className="bg-dark-green text-white w-[30%] px-4 py-2 mt-6 mx-2 rounded-md hover:bg-yinmn-blue" 
+                type="button"
+                onClick={submitEntry}
+              >
+                Create Entry
+              </button>
             </div>
             <p className="my-2 mx-6 text-sm text-red-600">{errorMessage}</p>
           </form>
