acc8fcfd13608402dfa9071fbc8d72751c933983
Daniel Gutkin <dngutkin@gmail.com>
2024-02-29 19:05:43 -0500

    Set min width for login div

--- app/login/page.js
+++ app/login/page.js
@@ -49,7 +49,7 @@ export default function Login() {
                 <Loader/>
                 :
 
-                <div className="bg-white p-8 border border-gray-200 rounded-lg shadow-md">
+                <div className="p-8 border border-gray-200 rounded-lg shadow-md min-w-fit min-h-fit">
 
                     <h2 className="text-2xl font-semibold mb-6">Login</h2>
                     
