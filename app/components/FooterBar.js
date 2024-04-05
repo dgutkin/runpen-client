e818eec546fec462196122459180d8fd19b2ad7c
Daniel Gutkin <dngutkin@gmail.com>
2024-04-05 16:05:05 -0400

    Change footer message to mention encryption

--- app/components/FooterBar.js
+++ app/components/FooterBar.js
@@ -5,7 +5,7 @@ export default function FooterBar() {
     return(
       <div className="bg-dark-green py-4 px-6 xl:px-36 w-full fixed bottom-0 z-10">
         <div className="flex flex-row justify-between mx-4">
-          <p className="text-white text-xs">An indie development project.</p>
+          <p className="text-white text-xs">Privacy matters. runPen is AES encrypted.</p>
         </div>
       </div>
     );
