98ddb395d340f7590c50a4df5f61cf3e71793fd1
Daniel Gutkin <dngutkin@gmail.com>
2024-04-23 11:37:11 -0400

    Rename create-account route to signup

--- app/page.js
+++ app/page.js
@@ -38,7 +38,7 @@ export default function Home() {
               </Link>
               <Link 
                 className="bg-dark-green text-white px-4 py-2 rounded-md hover:bg-yinmn-blue" 
-                href="/create-account"
+                href="/signup"
               >
                 Sign Up
               </Link>
